156c43e86b1232220d5a69f26a3fd21e61c4a460
Prashant Pawar <prashantpawar@prashantpawar.com>
2009-11-28 00:47:38 -0500

    Added database details.
    Signed-off-by: Prashant Pawar <prashantpawar@prashantpawar.com>

--- application/config/database.php
+++ application/config/database.php
@@ -37,10 +37,10 @@
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['hostname'] = "renegade.xen.prgmr.com";
+$db['default']['username'] = "okdamdev";
+$db['default']['password'] = "chimpanzee";
+$db['default']['database'] = "okamdev";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
