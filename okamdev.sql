3ed918fa84ca1a7b182b436455236d5d506b99dd
Prashant Pawar <prashantpawar@prashantpawar.com>
2009-11-28 17:18:06 -0500

    Added Adam griffith's authentication library
    Signed-off-by: Prashant Pawar <prashantpawar@prashantpawar.com>

--- okamdev.sql
+++ okamdev.sql
@@ -4,7 +4,7 @@ Source Host: 68.68.97.113
 Source Database: okamdev
 Target Host: 68.68.97.113
 Target Database: okamdev
-Date: 11/28/2009 5:03:56 PM
+Date: 11/28/2009 5:14:05 PM
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -17,6 +17,19 @@ CREATE TABLE `ok_answers` (
   `answer_text` text,
   `question_id` int(11) default NULL,
   PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for ok_ci_sessions
+-- ----------------------------
+DROP TABLE IF EXISTS `ok_ci_sessions`;
+CREATE TABLE `ok_ci_sessions` (
+  `session_id` varchar(40) NOT NULL default '0',
+  `ip_address` varchar(16) NOT NULL default '0',
+  `user_agent` varchar(50) NOT NULL,
+  `last_activity` int(10) unsigned NOT NULL default '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY  (`session_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- ----------------------------
@@ -35,6 +48,17 @@ CREATE TABLE `ok_dnas` (
   `answer4_id` int(11) default NULL,
   `answer5_id` int(11) default NULL,
   `answer6_id` int(11) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for ok_groups
+-- ----------------------------
+DROP TABLE IF EXISTS `ok_groups`;
+CREATE TABLE `ok_groups` (
+  `id` int(11) NOT NULL,
+  `title` varchar(20) NOT NULL default '',
+  `description` varchar(100) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -74,6 +98,21 @@ CREATE TABLE `ok_relevancies` (
   `relevancy` int(11) default NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for ok_users
+-- ----------------------------
+DROP TABLE IF EXISTS `ok_users`;
+CREATE TABLE `ok_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `group_id` int(11) NOT NULL default '100',
+  `token` varchar(255) NOT NULL,
+  `identifier` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records 
